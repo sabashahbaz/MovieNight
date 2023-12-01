132a98a766fd26e6f35e989fdd97a0d0cdf8004c
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-12-01 12:41:34 -0500

    slider change

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -2,9 +2,16 @@ import {useEffect, useState} from 'react'
 import axios from 'axios'
 import {HiChevronLeft, HiChevronRight} from "react-icons/hi2";
 
+//defining interface named TrendingMovies to specify the structure of a single movie
+//poster_path is a string
+interface TrendingMovies {
+    poster_path: string;
+}
+
+
 const Slider = () => {
     const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
-    const [trendingMovies, setTrendingMovies] = useState([])
+    const [trendingMovies, setTrendingMovies] = useState<TrendingMovies[]>([])
 
     useEffect(() => {
         axios.get('/trending_movies')
@@ -18,12 +25,12 @@ const Slider = () => {
 
     return (
         <div>
-            <HiChevronLeft className="text-red text-[30px] absolute mx-8 mt-[150px] cursor-pointer"/>
-            <HiChevronRight className="text-red text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"/>
+            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"/>
+            <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"/>
         
-        <div className = 'flex overflow-x-auto w-full px-16 py-4 scrollbar-hide'>
-            {trendingMovies.map((item) => (
-            <img src ={IMAGE_BASE_URL+item.poster_path} className = 'min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md'/>
+        <div className = 'flex overflow-x-auto w-full px-4 py-4 scrollbar-hide'>
+            {trendingMovies.map((movie) => (
+            <img src ={IMAGE_BASE_URL+movie.poster_path} className = 'w-[310px] md:h-[450px] mr-5 rounded-md'/>
         ))}
         </div>
         </div>
